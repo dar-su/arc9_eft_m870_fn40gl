c7e70c6e5f643643174e2b63721e011253c16db2
Darsu <95997959+dar-su@users.noreply.github.com>
2024-11-09 21:47:00 +0700

    volume

--- lua/weapons/arc9_eft_fn40gl.lua
+++ lua/weapons/arc9_eft_fn40gl.lua
@@ -271,6 +271,10 @@ SWEP.BulletBones  = { -- the bone that represents bullets in gun/mag
 
 local path = "weapons/darsu_eft/fn40/"
 
+SWEP.ShootPitchVariation = 0
+SWEP.DistantShootVolume = 0.05
+SWEP.DistantShootVolumeActual = 0.05
+
 SWEP.ShootSound = path .. "fn40gl_fire_close.ogg"
 SWEP.ShootSoundIndoor = path .. "fn40gl_fire_indoor_close.ogg"
 SWEP.DistantShootSound = path .. "fn40gl_fire_distant.ogg"
