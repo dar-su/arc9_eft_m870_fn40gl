6c22172b94280d1eadd87fff7029f31e29988a24
Darsu <95997959+dar-su@users.noreply.github.com>
2025-05-07 14:14:03 +0700

    tpik angle fix

--- lua/weapons/arc9_eft_fn40gl.lua
+++ lua/weapons/arc9_eft_fn40gl.lua
@@ -30,7 +30,7 @@ SWEP.WorldModelOffset = {
     Pos = Vector(-10, 5.5, -6),
     Ang = Angle(-7, 0, 180),
     TPIKPos = Vector(-8, 6, -1), -- rpg
-    TPIKAng = Angle(-5, 0, 180),
+    TPIKAng = Angle(-11.5, 0, 180),
     Scale = 1
 }
 
